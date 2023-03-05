7f28feb5c4727ce12c92acbe483921f73a2be22d
Cristianquis <126981245+Cristianquis@users.noreply.github.com>
2023-03-05 11:04:40 -0300

    Initial commit

---
+++ Cvcris.js
@@ -0,0 +1,4 @@
+document.getElementById("boton").onclick = function () {
+    console.log("Capturamos el evento click")
+    document.getElementById("demo").innerHTML = "Ponerse en conctaco al 1166841647"; 
+}
